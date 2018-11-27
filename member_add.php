cca051083cc822d5ba1ef4d07181cf59e5718c9b
Brazilian114 <koravit114@gmail.com>
2018-11-27 20:27:33 +0700

    first commit

--- member_add.php
+++ member_add.php
@@ -184,21 +184,6 @@
 
 
 
-<!--<li class="nav-item dropdown">
-<a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-<i class="fas fa-fw fa-folder"></i>
-<span>เพิ่มรายการคิว</span>
-</a>
-<div class="dropdown-menu" aria-labelledby="pagesDropdown">
-<h6 class="dropdown-header">เมนูหลัก :</h6>
-<a class="dropdown-item" href="queue_walkin_form.php">เพิ่มคิวล้างรถลูกค้า</a>
-<a class="dropdown-item" href="service_insert_form.php">เพิ่มรูปแบบบริการ</a>
-<div class="dropdown-divider"></div>
-<h6 class="dropdown-header">รายการอื่นๆ :</h6>
-<a class="dropdown-item" href="member_add.php">เพิ่มลูกค้าประจำ</a>
-<a class="dropdown-item" href="promo.php">แก้ไขโปโมชั่น</a>
-</div>
-</li>-->
 
 
 
